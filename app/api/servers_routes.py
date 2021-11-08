8c5c16cf0fac9da04b253d5d8ebc8004520e83fa
Joey Peterson <joey.peterson15@gmail.com>
2021-11-08 10:35:04 -0800

    get and create servers

---
+++ app/api/servers_routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, session, request, redirect
+from app.models import Server, db
+from app.forms import ServerForm
+
+servers_routes = Blueprint('servers', __name__)
+
+@servers_routes.route('/')
+def get_servers():
+    servers = Server.query.all()
+    return {'servers': [server.to_dict() for server in servers]}
+
+@servers_routes.route('/', methods = ['POST'])
+def add_server():
+
+    form = ServerForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+
+        server = Server(
+            name = form.data['name'],
+            ownerId=form.data['owner_id']
+        )
+
+    # server = Server(name='fred', owner_id=1)
+    db.session.add(server)
+    db.session.commit()
+    return server.to_dict()
